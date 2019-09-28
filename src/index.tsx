7e75dfbfe9d6a492a20b55a9f5f65a86c1389de1
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-28 15:28:04 +0530

    Pre-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from "react-dom"
 
-export interface Props {
+export interface AppProps {
     color: string
     shape?: string
 }
  
-export interface State {
+export interface AppState {
     counter?: number
 }
- 
-class App extends React.Component<Props, State> {
+
+/**
+ * In the Component class, the state is defined as `state: Readonly<S>;`. This has implications :
+ * So if inside the constructor `this.state` is set, then it ends up setting the value of the 
+ * `state` property defined in the parent Component. Also `S` matches with the state type generic
+ * being passed in, so the `state` property would be the value set in the constructor with 
+ * it's type being `Readonly<AppState>`. 
+ * BUT, if you define state as a class property in the manner `state = <Your state here>`, then
+ * what you are ending up doing here is that you are over-riding the parent's state property completely
+ * and that is no longer `Readonly<S>` but is the value you had defined during the initialization. And,
+ * as a result the state generic being passed in also loses it's significance. 
+ */
+class App extends React.Component<AppProps> {
 
     state  = {counter: 0}
+
+    constructor(props: AppProps) {
+        super(props)
+        //this.state = {counter: 0}
+    }
 
     onIncrement = (): void => {
         this.setState({ counter: this.state.counter + 1 });
@@ -21,6 +37,7 @@ class App extends React.Component<Props, State> {
     onDecrement = (): void => {
         this.setState({ counter: this.state.counter - 1 });
     };
+    
     render() { 
         return ( 
             <div>
@@ -32,5 +49,9 @@ class App extends React.Component<Props, State> {
          );
     }
 }
+
+/* const App = (props:AppProps): JSX.Element => {
+    return <div>{props.color}</div>
+} */
 
 ReactDOM.render(<App color="red"/>, document.querySelector('#root'))
